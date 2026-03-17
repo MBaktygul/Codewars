cf3e833b90e340f4fa2f24fbcb3fd5de704ba0a8
Baktygul <167072757+MBaktygul@users.noreply.github.com>
2026-03-17 14:53:53 +0100

    Add solution - CodeHub

---
+++ 8-kyu/switch-it-up/switch-it-up.js
@@ -0,0 +1,31 @@
+function switchItUp(number){
+  switch (number) {
+    case 0:
+      return 'Zero';
+      break;
+  }
+  switch (number) {
+    case 1:
+      return 'One';
+      break;
+  }
+    switch (number) {
+    case 2:
+      return 'Two';
+      break;
+  }
+    switch (number) {
+    case 3:
+      return 'Three';
+      break;
+  }
+    switch (number) {
+    case 4:
+      return 'Four';
+      break;
+  }
+    switch (number) {
+    case 5:
+      return 'Five';
+      break;
+  }
