73e10753fb13e2c34227c3dd1b05f6e5420619c4
Baktygul <167072757+MBaktygul@users.noreply.github.com>
2026-06-04 10:36:56 +0200

    Add solution - CodeHub

---
+++ 8-kyu/calculate-average/calculate-average.js
@@ -0,0 +1,13 @@
+function findAverage(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+​
+  let sum = 0;
+​
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+​
+  return sum / array.length;
+}
