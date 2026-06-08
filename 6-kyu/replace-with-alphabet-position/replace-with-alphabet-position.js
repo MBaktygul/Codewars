4c703eda602bcf2356a972923585e2b7dab77f5f
Baktygul <167072757+MBaktygul@users.noreply.github.com>
2026-06-08 11:26:51 +0200

    Add solution - CodeHub

---
+++ 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.js
@@ -0,0 +1,13 @@
+function alphabetPosition(text) {
+  let result = [];
+​
+  for (let char of text.toLowerCase()) {
+    if (char >= 'a' && char <= 'z') {
+      result.push(char.charCodeAt(0) - 96);
+    }
+  }
+​
+  return result.join(' ');
+}
+​
+console.log(alphabetPosition('love'));      // 12 15 22 5
